f28d0fb832e93067e0523140260649abcc37bc6e
Seohyeon Kim <janey@Seohyeons-MacBook-Pro.local>
2024-08-06 21:09:07 +0900

    다 합쳣

--- lib/screens/03_favorite/favoriate.dart
+++ lib/screens/03_favorite/favoriate.dart
@@ -11,14 +11,16 @@ class Favorite extends StatelessWidget {
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
+        children: const [
           Option(
             imageUrl: 'assets/fav1.png',
             name: '오뜨로 성수',
+            category: '식당',
           ),
           Option(
             imageUrl: 'assets/fav2.png',
             name: '차이나플레인',
+            category: '식당',
           ),
         ],
       ),
@@ -26,17 +28,52 @@ class Favorite extends StatelessWidget {
   }
 }
 
-class Option extends StatelessWidget {
+class Option extends StatefulWidget {
   final String imageUrl;
   final String name;
-  Option({super.key, required this.imageUrl, required this.name});
+  final String category;
+
+  const Option(
+      {super.key,
+      required this.imageUrl,
+      required this.name,
+      required this.category});
+
+  @override
+  _OptionState createState() => _OptionState();
+}
+
+class _OptionState extends State<Option> {
+  bool _isFavorited = false;
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorited = !_isFavorited;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset(imageUrl, width: 150, height: 150),
-        Icon(Icons.favorite, color: Color(0xff4863E0)),
-        Text(name, style: TextStyle(fontSize: 16)),
+        Stack(
+          children: [
+            Image.asset(widget.imageUrl, width: 150, height: 150),
+            Positioned(
+              bottom: 3,
+              right: 3,
+              child: IconButton(
+                onPressed: _toggleFavorite,
+                icon: Icon(Icons.favorite,
+                    color: _isFavorited ? Color(0xff4863E0) : Colors.grey),
+                iconSize: 27,
+              ),
+            ),
+          ],
+        ),
+        Text(widget.name, style: TextStyle(fontSize: 20, color: Colors.black)),
+        Text(widget.category,
+            style: TextStyle(fontSize: 16, color: Colors.grey)),
       ],
     );
   }
--- lib/screens/05_detailed/info.dart
+++ lib/screens/05_detailed/info.dart
@@ -2,35 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/05_detailed/review.dart';
 import 'package:flutter_application_1/screens/05_detailed/scroll1.dart';
 
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({super.key});
-
-  @override
-  _FavoriteButtonState createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool _isFavorited = false;
-
-  void _toggleFavorite() {
-    setState(() {
-      _isFavorited = !_isFavorited;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: _toggleFavorite,
-      icon: Icon(
-        Icons.favorite,
-        color: _isFavorited ? Color(0xff4863E0) : Colors.grey,
-      ),
-      iconSize: 27,
-    );
-  }
-}
-
 class Info extends StatefulWidget {
   const Info({super.key});
 
